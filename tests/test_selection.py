f2807c9495bb6a4e335ddbafbe8d59c9eb4e701e
salotz <salotz@salotz.info>
2016-07-28 13:35:53 -0400

    Started unittests

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,4 +1,40 @@
 import doctest
-from mast import selection
+import unittest
 
-nfail, ntests = doctest.testmod(selection, verbose=True)
+from mast import selection
+import mast.selection as mastsel
+
+class TestSelectionMember(unittest.TestCase):
+    def setUp(self):
+        self.member = 'a'
+        self.selection_member = mastsel.SelectionMember(self.member)
+
+    def tearDown(self):
+        pass
+
+    def test_constructor(self):
+        pass
+
+    def test_member(self):
+        self.assertEqual(self.selection_member.member, self.member)
+
+    def test_registry(self):
+        self.assertEqual(self.selection_member.registry, [])
+
+    def test_repr(self):
+        pass
+
+    def test_get_selections(self):
+        self.selection_member.get_selections()
+
+    def test_register_selection(self):
+        pass
+
+if __name__ == "__main__":
+    # doctests
+    print("\n\n\n Doc Tests\n-----------")
+    nfail, ntests = doctest.testmod(selection, verbose=True)
+
+    # unit tests
+    print("\n\n\n Unit Tests\n-----------")
+    unittest.main()
