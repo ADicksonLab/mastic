74a7e27bef2ec260e5dd00f1893deb2fe9eb5e5c
salotz <salotz@salotz.info>
2017-11-02 11:45:34 -0400

    refactored the module to be similar to other projects
    moved everything into the mastic subfolder and updated the setup.py script

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MAST',
+    name='mastic',
+    version='0.2.1-beta',
+    py_modules=['mastic']
     author='Samuel D. Lotz',
     author_email='samuel.lotz@salotz.info',
     packages=find_packages(),
